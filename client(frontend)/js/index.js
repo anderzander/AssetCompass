8e37e8550e214a947479643aed65ab77dc649b52
Lorberski <lorenz.rettenwender@stud.fh-campuswien.ac.at>
2024-05-29 18:17:36 +0200

    Basic currency converter gets rate from API

--- client(frontend)/js/index.js
+++ client(frontend)/js/index.js
@@ -1,5 +1,25 @@
 
-module.exports = function() {
-    console.log('index.js wird ausgeführt');
-};
+
+function convert(){
+    const xhr = new XMLHttpRequest();
+
+    xhr.onload = function (){
+        if (xhr.status === 200) {
+            var amount = document.getElementById('converterBTC').value;
+            var convertedValue = document.getElementById('output')
+            var bitcoinValue = JSON.parse(xhr.responseText)
+            convertedValue.innerText = (bitcoinValue.EUR * amount).toFixed(2) + " €";
+        } else {
+            const error = 'Error in loadCurrency()'
+            return error;
+        }
+    }
+
+    const url = new URL("/currency", location.href)
+
+    xhr.open("GET", url)
+    xhr.send()
+}
+
+
 
