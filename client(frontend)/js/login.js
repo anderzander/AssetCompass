780d4f03f820438c6212ed9e3cbbc0578b0b3752
PatiPfa <131695967+PatiPfa@users.noreply.github.com>
2024-05-29 19:59:09 +0200

    comfirm field

--- client(frontend)/js/login.js
+++ client(frontend)/js/login.js
@@ -4,18 +4,22 @@ let signupBtn = document.getElementById("signupBtn");
 let signinBtn = document.getElementById("signinBtn");
 let nameField = document.getElementById("nameField");
 let title = document.getElementById("title");
+let confirmPasswordField = document.getElementById('confirmPasswordField');
+
 
 signinBtn.onclick = function (){
     nameField.style.maxHeight = "0";
     title.innerHTML = "Sign In";
     signupBtn.classList.add("disable");
     signinBtn.classList.remove("disable");
+    confirmPasswordField.style.maxHeight= "0";
 }
 signupBtn.onclick = function (){
     nameField.style.maxHeight = "60px";
     title.innerHTML = "Sign Up";
     signupBtn.classList.remove("disable");
     signinBtn.classList.add("disable");
+    confirmPasswordField.style.maxHeight = "60px";
 }
 
 
