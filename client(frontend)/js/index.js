954ec37dafdc52a6a9be34ab4297d38dfe600c0d
Lorberski <lorenz.rettenwender@stud.fh-campuswien.ac.at>
2024-06-03 12:34:06 +0200

    before merge

--- client(frontend)/js/index.js
+++ client(frontend)/js/index.js
@@ -1,33 +1,33 @@
 import {ElementBuilder, ParentChildBuilder} from './builders.js';
 
 
-document.addEventListener('DOMContentLoaded', () => {
-    fetch('https://min-api.cryptocompare.com/data/price?fsym=BNB&tsyms=USD,JPY,EUR&api_key=db34a3799a2813ae847aa1145cdd903b1b7fc3aec63c1c1a1eec59435ca95c4c')
-        .then(response => response.json())
-        .then(data => {
-            const boxTitle = document.getElementById('box-1-title');
-            const boxContent = document.getElementById('box-1-content');
-
-            // Update Title
-            boxTitle.textContent = 'BNB Prices';
-
-            // Update Content
-            boxContent.innerHTML = `
-                <p>USD: ${data.USD}</p>
-                <p>JPY: ${data.JPY}</p>
-                <p>EUR: ${data.EUR}</p>
-            `;
-        })
-        .catch(error => console.error('Error fetching data:', error));
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//     fetch('https://min-api.cryptocompare.com/data/price?fsym=BNB&tsyms=USD,JPY,EUR&api_key=db34a3799a2813ae847aa1145cdd903b1b7fc3aec63c1c1a1eec59435ca95c4c')
+//         .then(response => response.json())
+//         .then(data => {
+//             const boxTitle = document.getElementById('box-1-title');
+//             const boxContent = document.getElementById('box-1-content');
+//
+//             // Update Title
+//             boxTitle.textContent = 'BNB Prices';
+//
+//             // Update Content
+//             boxContent.innerHTML = `
+//                 <p>USD: ${data.USD}</p>
+//                 <p>JPY: ${data.JPY}</p>
+//                 <p>EUR: ${data.EUR}</p>
+//             `;
+//         })
+//         .catch(error => console.error('Error fetching data:', error));
+// });
 
 
 // Trigger convert button click on Enter key press in the input field
-document.getElementById('converterBTC').addEventListener('keypress', function (event) {
-    if (event.key === 'Enter') {
-        event.preventDefault(); // Prevent form submission
-    }
-});
+// document.getElementById('converterBTC').addEventListener('keypress', function (event) {
+//     if (event.key === 'Enter') {
+//         event.preventDefault(); // Prevent form submission
+//     }
+// });
 
 
 //add a new Box
