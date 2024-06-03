53fd2b202a3938c54870fc0df11197fe985c1b72
anderzander <afaenderfake@gmail.com>
2024-06-03 11:49:27 +0200

    newest version

--- server(backend)/assetModels.js
+++ server(backend)/assetModels.js
@@ -1,15 +1,15 @@
-const cryptoAssets = {
-    BTC: {id: 'BTC', name: 'Bitcoin'},
-    ETH: {id: 'ETH', name: 'Ethereum'},
-    BNB: {id: 'BNB', name: 'Binance Coin'},
-    XRP: {id: 'XRP', name: 'Ripple'},
-    ADA: {id: 'ADA', name: 'Cardano'},
-    DOGE: {id: 'DOGE', name: 'Dogecoin'},
-    DOT: {id: 'DOT', name: 'Polkadot'},
-    UNI: {id: 'UNI', name: 'Uniswap'},
-    LTC: {id: 'LTC', name: 'Litecoin'},
-    LINK: {id: 'LINK', name: 'Chainlink'}
-};
+// const cryptoAssets = {
+//     BTC: {id: 'BTC', name: 'Bitcoin'},
+//     ETH: {id: 'ETH', name: 'Ethereum'},
+//     BNB: {id: 'BNB', name: 'Binance Coin'},
+//     XRP: {id: 'XRP', name: 'Ripple'},
+//     ADA: {id: 'ADA', name: 'Cardano'},
+//     DOGE: {id: 'DOGE', name: 'Dogecoin'},
+//     DOT: {id: 'DOT', name: 'Polkadot'},
+//     UNI: {id: 'UNI', name: 'Uniswap'},
+//     LTC: {id: 'LTC', name: 'Litecoin'},
+//     LINK: {id: 'LINK', name: 'Chainlink'}
+// };
 
 
 const assetsInUse = {};
@@ -105,4 +105,4 @@ const allAssets = {
 
 };
 
-module.exports = {cryptoAssets, assetsInUse, allAssets};
+module.exports = { assetsInUse, allAssets};
